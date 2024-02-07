05ccbed5589cdc88e8c0a4b9aa1ed5c60499ad9f
divyamraj1301 <divyamraj1301@gmail.com>
2024-02-07 14:30:52 +0530

    ready for build

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -4,6 +4,7 @@ import Layout from "./../components/Layout/Layout";
 import { useCart } from "../context/cart";
 import { useAuth } from "../context/auth";
 import { useNavigate } from "react-router-dom";
+import { message } from "antd";
 import DropIn from "braintree-web-drop-in-react";
 
 const CartPage = () => {
